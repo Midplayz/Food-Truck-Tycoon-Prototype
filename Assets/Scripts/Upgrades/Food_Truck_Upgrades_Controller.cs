95e39086a5cb974f090a4e274634dfd9f4476a85
Midplayz <88376046+Midplayz@users.noreply.github.com>
2025-01-24 16:32:59 +0530

    Basic Purchase Script

--- Assets/Scripts/Upgrades/Food_Truck_Upgrades_Controller.cs
+++ Assets/Scripts/Upgrades/Food_Truck_Upgrades_Controller.cs
@@ -1,20 +1,54 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Food_Truck_Upgrades_Controller : MonoBehaviour
 {
-    [field: Header("Rotation Values")]
-    [field: SerializeField] private float autoRotationSpeed = 10f;
-    [field: SerializeField] float dragRotationSpeed = 5f; 
-    private bool isDragging = false; 
-    private float dragStartX; 
+    [Header("Rotation Values")]
+    [SerializeField] private float autoRotationSpeed = 10f;
+    [SerializeField] private float dragRotationSpeed = 5f;
+    private bool isDragging = false;
     private float rotationY;
 
-    [field: Header("Rotation Values")]
-    [field: SerializeField] private GameObject rustedTruck;
-    [field: SerializeField] private GameObject cleanTruck;
-    [field: SerializeField] private GameObject sushiTopper;
-    [field: SerializeField] private GameObject roof;
-    [field: SerializeField] private GameObject chef;
+    [Header("Truck Upgrades")]
+    [SerializeField] private GameObject rustedTruck;
+    [SerializeField] private GameObject cleanTruck;
+    [SerializeField] private GameObject sushiTopper;
+    [SerializeField] private GameObject roof;
+    [SerializeField] private GameObject chef;
+
+    [Header("Upgrade Prices and Buttons")]
+    [SerializeField] private int truckUpgradePrice = 50;
+    [SerializeField] private int topperPrice = 30;
+    [SerializeField] private int roofPrice = 40;
+    [SerializeField] private int chefPrice = 70;
+
+    [SerializeField] private Button truckButton;
+    [SerializeField] private Button topperButton;
+    [SerializeField] private Button roofButton;
+    [SerializeField] private Button chefButton;
+
+    [SerializeField] private TextMeshProUGUI truckButtonText;
+    [SerializeField] private TextMeshProUGUI topperButtonText;
+    [SerializeField] private TextMeshProUGUI roofButtonText;
+    [SerializeField] private TextMeshProUGUI chefButtonText;
+
+    [SerializeField] private int playerMoney = 100;
+
+    private bool truckPurchased = false;
+    private bool topperPurchased = false;
+    private bool roofPurchased = false;
+    private bool chefPurchased = false;
+
+    private void Start()
+    {
+        UpdateUI();
+
+        truckButton.onClick.AddListener(() => PurchaseUpgrade("Truck"));
+        topperButton.onClick.AddListener(() => PurchaseUpgrade("Topper"));
+        roofButton.onClick.AddListener(() => PurchaseUpgrade("Roof"));
+        chefButton.onClick.AddListener(() => PurchaseUpgrade("Chef"));
+    }
 
     private void Update()
     {
@@ -27,18 +61,77 @@ public class Food_Truck_Upgrades_Controller : MonoBehaviour
         else
         {
             transform.Rotate(0, autoRotationSpeed * Time.deltaTime, 0, Space.World);
-            rotationY = transform.rotation.eulerAngles.y; 
+            rotationY = transform.rotation.eulerAngles.y;
         }
     }
 
     private void OnMouseDown()
     {
         isDragging = true;
-        dragStartX = Input.mousePosition.x;
     }
 
     private void OnMouseUp()
     {
         isDragging = false;
+    }
+
+    private void PurchaseUpgrade(string upgrade)
+    {
+        switch (upgrade)
+        {
+            case "Truck":
+                if (!truckPurchased && playerMoney >= truckUpgradePrice)
+                {
+                    playerMoney -= truckUpgradePrice;
+                    truckPurchased = true;
+                    rustedTruck.SetActive(false);
+                    cleanTruck.SetActive(true);
+                }
+                break;
+
+            case "Topper":
+                if (!topperPurchased && playerMoney >= topperPrice)
+                {
+                    playerMoney -= topperPrice;
+                    topperPurchased = true;
+                    sushiTopper.SetActive(true);
+                }
+                break;
+
+            case "Roof":
+                if (!roofPurchased && playerMoney >= roofPrice)
+                {
+                    playerMoney -= roofPrice;
+                    roofPurchased = true;
+                    roof.SetActive(true);
+                }
+                break;
+
+            case "Chef":
+                if (!chefPurchased && playerMoney >= chefPrice)
+                {
+                    playerMoney -= chefPrice;
+                    chefPurchased = true;
+                    chef.SetActive(true);
+                }
+                break;
+        }
+
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        truckButtonText.text = truckPurchased ? "Purchased" : $"Buy for ${truckUpgradePrice}";
+        truckButton.interactable = !truckPurchased && playerMoney >= truckUpgradePrice;
+
+        topperButtonText.text = topperPurchased ? "Purchased" : $"Buy for ${topperPrice}";
+        topperButton.interactable = !topperPurchased && playerMoney >= topperPrice;
+
+        roofButtonText.text = roofPurchased ? "Purchased" : $"Buy for ${roofPrice}";
+        roofButton.interactable = !roofPurchased && playerMoney >= roofPrice;
+
+        chefButtonText.text = chefPurchased ? "Purchased" : $"Buy for ${chefPrice}";
+        chefButton.interactable = !chefPurchased && playerMoney >= chefPrice;
     }
 }
