39214604023797d18b7b1302a72884906dd4fe44
Srivishnu Srinivasan <midnightassassinmc@gmail.com>
2025-03-07 15:46:05 +0530

    Finished Pre-Day Stuff

--- Assets/Scripts/PreDay/PreDayPrep.cs
+++ Assets/Scripts/PreDay/PreDayPrep.cs
@@ -2,16 +2,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using TMPro;
-
-public enum LocationSelection
-{
-    UniversalStudios,
-    ElNido
-}
+using static UnityEditor.FilePathAttribute;
 
 public class SavedValues
 {
-    public static LocationSelection selectedLocation;
+    public static int selectedLocation;
     public static int startTime;
     public static int endTime;
     public static List<MenuItem> menuList;
@@ -45,8 +40,9 @@ public class PreDayPrep : MonoBehaviour
     [SerializeField] private GameObject ingredientsPurchasing;
 
     [field: Header("Location Selection")]
-    [SerializeField] private Button locationButton1;
-    [SerializeField] private Button locationButton2;
+    public List<LocationStats> locationStats = new List<LocationStats>();
+    [SerializeField] private GameObject locationPrefab;
+    [SerializeField] private GameObject locationView;
 
     [field: Header("Work Timings")]
     [SerializeField] private TMP_Dropdown startTimeDropdown;
@@ -62,7 +58,16 @@ public class PreDayPrep : MonoBehaviour
 
     [field: Header("Ingredients Purchasing")]
     [SerializeField] private ScrollRect ingredientsScrollRect;
-
+    [SerializeField] private GameObject ingredientsItemPrefab;
+    [SerializeField] private Button ingredientsContinueButton;
+    [SerializeField] private GameObject RestockPanel;
+    [SerializeField] private TextMeshProUGUI restockTitle;
+    [SerializeField] private TextMeshProUGUI itemInfo;
+    [SerializeField] private TextMeshProUGUI cashInHand;
+    [SerializeField] private TMP_InputField quantityInput;
+    [SerializeField] private Button confirmRestockButton;
+    [SerializeField] private Button cancelRestockButton;
+    public List<IngredientItem> ingredientsList = new List<IngredientItem>();
 
     private void Awake()
     {
@@ -84,9 +89,6 @@ public class PreDayPrep : MonoBehaviour
         menuPlans.SetActive(false);
         ingredientsPurchasing.SetActive(false);
 
-        locationButton1.onClick.AddListener(() => SelectLocation(LocationSelection.UniversalStudios));
-        locationButton2.onClick.AddListener(() => SelectLocation(LocationSelection.ElNido));
-
         startTimeDropdown.onValueChanged.AddListener(delegate { ValidateWorkTimings(); });
         endTimeDropdown.onValueChanged.AddListener(delegate { ValidateWorkTimings(); });
         submitTimingsButton.onClick.AddListener(SubmitWorkTimings);
@@ -104,10 +106,38 @@ public class PreDayPrep : MonoBehaviour
     {
         title.text = "Select Location";
         preDayPanel.SetActive(true);
+        PopulateLocation();
         locationSelection.SetActive(true);
     }
 
-    private void SelectLocation(LocationSelection location)
+    private void PopulateLocation()
+    {
+        foreach (Transform child in locationView.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < locationStats.Count; i++)
+        {
+            LocationStats location = locationStats[i];
+
+            GameObject locationItem = Instantiate(locationPrefab, locationView.transform);
+
+            TextMeshProUGUI locationText = locationItem.GetComponentInChildren<TextMeshProUGUI>();
+            location.selectButton = locationItem.GetComponentInChildren<Button>();
+
+            locationText.text = $"{location.name}: {location.description}. " +
+                                $"Income Multiplier: {location.incomeMultiplier}x. " +
+                                $"Customers Multiplier: {location.customerSpawnRateMultipler}x. " +
+                                $"Competition: {location.competitionMultiplier}x. " +
+                                $"Crime: {location.crimeMultiplier}x.";
+
+            int index = i;
+            location.selectButton.onClick.AddListener(() => SelectLocation(index));
+        }
+    }
+
+    private void SelectLocation(int location)
     {
         Debug.Log("Location " + location + " selected");
         SavedValues.selectedLocation = location;
@@ -162,37 +192,146 @@ public class PreDayPrep : MonoBehaviour
 
             itemText.text = item.name;
             itemToggle.isOn = item.isActive;
-
-            if (item.price > 0)
-            {
-                priceInput.text = item.price.ToString();
-            }
-            else
-            {
-                priceInput.text = "";
-            }
+            priceInput.text = item.price > 0 ? item.price.ToString() : "";
 
             menuToggles.Add(itemToggle);
+
+            itemToggle.onValueChanged.AddListener(delegate { ValidateMenuSelection(); });
+            priceInput.onValueChanged.AddListener(delegate { ValidateMenuSelection(); });
         }
 
         menuContinueButton.interactable = false;
-        foreach (Toggle toggle in menuToggles)
-        {
-            toggle.onValueChanged.AddListener(delegate { ValidateMenuSelection(); });
-        }
+        menuContinueButton.onClick.AddListener(ConfirmMenu);
     }
 
     private void ValidateMenuSelection()
     {
-        foreach (Toggle toggle in menuToggles)
+        for (int i = 0; i < menuToggles.Count; i++)
         {
-            if (toggle.isOn)
+            Toggle toggle = menuToggles[i];
+            TMP_InputField priceInput = menuScrollRect.content.GetComponentsInChildren<TMP_InputField>()[i];
+
+            if (toggle.isOn && (string.IsNullOrEmpty(priceInput.text) || int.Parse(priceInput.text) <= 0))
             {
-                menuContinueButton.interactable = true;
+                menuContinueButton.interactable = false;
                 return;
             }
         }
-        menuContinueButton.interactable = false;
+
+        menuContinueButton.interactable = menuToggles.Exists(t => t.isOn);
+    }
+
+    private void ConfirmMenu()
+    {
+        Debug.Log("Menu Confirmed!");
+
+        SavedValues.menuList = new List<MenuItem>();
+
+        for (int i = 0; i < menuList.Count; i++)
+        {
+            menuList[i].isActive = menuToggles[i].isOn;
+            TMP_InputField priceInput = menuScrollRect.content.GetComponentsInChildren<TMP_InputField>()[i];
+
+            if (!string.IsNullOrEmpty(priceInput.text) && int.TryParse(priceInput.text, out int price) && price > 0)
+            {
+                menuList[i].price = price;
+
+                if (menuList[i].isActive)
+                {
+                    SavedValues.menuList.Add(new MenuItem
+                    {
+                        name = menuList[i].name,
+                        isActive = menuList[i].isActive,
+                        price = menuList[i].price
+                    });
+                }
+            }
+        }
+
+        menuPlans.SetActive(false);
+        PopulateIngredients();
+        title.text = "Restock Ingredients";
+        ingredientsPurchasing.SetActive(true);
+    }
+
+    private void PopulateIngredients()
+    {
+        RestockPanel.SetActive(false);
+        foreach (Transform child in ingredientsScrollRect.content)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (IngredientItem ingredient in ingredientsList)
+        {
+            GameObject ingredientObject = Instantiate(ingredientsItemPrefab, ingredientsScrollRect.content);
+
+            TextMeshProUGUI nameText = ingredientObject.GetComponentInChildren<TextMeshProUGUI>();
+            Button restockButton = ingredientObject.GetComponentInChildren<Button>();
+
+            nameText.text = $"{ingredient.name} x{ingredient.currentQuantity}";
+
+            restockButton.onClick.AddListener(() => RestockButtonPressed(ingredient.ingredientReferance));
+        }
+
+        ingredientsContinueButton.interactable = true;
+        ingredientsContinueButton.onClick.AddListener(ConfirmIngredientsSelection);
+    }
+
+    private void RestockButtonPressed(Ingredients ingredient)
+    {
+        IngredientItem selectedIngredient = ingredientsList.Find(item => item.ingredientReferance == ingredient);
+        if (selectedIngredient == null) return;
+
+
+        restockTitle.text = $"Restock {selectedIngredient.name}";
+        itemInfo.text = $"MOQ: {selectedIngredient.moq} | ${selectedIngredient.price} Per Piece";
+        cashInHand.text = "Cash Balance: {Cash}";
+
+        quantityInput.text = "";
+        confirmRestockButton.interactable = false;
+
+        RestockPanel.SetActive(true);
+        
+        quantityInput.onValueChanged.RemoveAllListeners();
+        quantityInput.onValueChanged.AddListener(delegate {
+            int quantity;
+            if (int.TryParse(quantityInput.text, out quantity) && quantity >= selectedIngredient.moq)
+            {
+                confirmRestockButton.interactable = true;
+            }
+            else
+            {
+                confirmRestockButton.interactable = false;
+            }
+        });
+
+        confirmRestockButton.onClick.RemoveAllListeners();
+        confirmRestockButton.onClick.AddListener(() => ConfirmRestockPressed(ingredient, int.Parse(quantityInput.text)));
+
+        cancelRestockButton.onClick.RemoveAllListeners();
+        cancelRestockButton.onClick.AddListener(CancelRestockPressed);
+    }
+
+    private void ConfirmRestockPressed(Ingredients ingredient, int quantity)
+    {
+        IngredientItem selectedIngredient = ingredientsList.Find(item => item.ingredientReferance == ingredient);
+        if (selectedIngredient == null) return;
+        selectedIngredient.currentQuantity += quantity;
+        PopulateIngredients();
+        RestockPanel.SetActive(false);
+    }
+
+    private void CancelRestockPressed()
+    {
+        RestockPanel.SetActive(false);
+    }
+
+    private void ConfirmIngredientsSelection()
+    {
+        Debug.Log("Ingredients Confirmed!");
+        ingredientsPurchasing.SetActive(false);
+        preDayPanel.SetActive(false);
     }
 }
 
@@ -203,4 +342,27 @@ public class MenuItem
     public bool isActive;
     public List<Ingredients> ingredients;
     public int price;
+}
+
+[System.Serializable]
+public class LocationStats
+{
+    public string name;
+    public string description;
+    public float incomeMultiplier = 1.0f;
+    public float customerSpawnRateMultipler = 1.0f;
+    public float competitionMultiplier = 1.0f;
+    public float crimeMultiplier = 1.0f;
+    public Button selectButton;
+}
+
+[System.Serializable]
+public class IngredientItem
+{
+    public string name;
+    public int moq;
+    public int price;
+    public int currentQuantity;
+    public bool isPerishable;
+    public Ingredients ingredientReferance;
 }
